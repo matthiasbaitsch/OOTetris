c56d1533fcec58a36a1918869918ba95a4d7bf41
Matthias Baitsch <matthias.baitsch@me.com>
2026-04-08 17:57:54 +0200

    wip

--- OOTetris.Tests/BoardTest.cs
+++ OOTetris.Tests/BoardTest.cs
@@ -5,7 +5,7 @@ using OOTetris;
 public class BoardTest
 {
     [Fact]
-    public void TestFit()
+    public void TestCanMovePiece()
     {
         Board b = new Board();
         b.TryPutPiece(Piece.MakePiece(Piece.Type.O));
@@ -17,5 +17,42 @@ public class BoardTest
         Assert.False(b.CanMovePiece(0, Board.Width - 1));
         Assert.False(b.CanMovePiece(-1, 0));
         Assert.False(b.CanMovePiece(0, -1));
+    }
+
+    [Fact]
+    public void TestClearRowsEmpty()
+    {
+        Board board = new Board();
+        Assert.Equal(0, board.ClearRows());
+    }
+
+    [Fact]
+    public void TestClearRowsOneRow()
+    {
+        Board board = new Board();
+        board.TryPutPiece(Piece.MakePiece(Piece.Type.I));
+        board.MovePiece(19, -4);
+        board.Place();
+        board.TryPutPiece(Piece.MakePiece(Piece.Type.I));
+        board.MovePiece(19, 0);
+        board.Place();
+        board.TryPutPiece(Piece.MakePiece(Piece.Type.O));
+        board.MovePiece(18, 4);
+        board.Place();
+        Assert.Equal(1, board.ClearRows());
+    }
+
+    [Fact]
+    public void TestClearRowsTwoRows()
+    {
+        Board board = new Board();
+
+        for (int col = 0; col < Board.Width; col += 2)
+        {
+            board.TryPutPiece(Piece.MakePiece(Piece.Type.O));
+            board.MovePiece(Board.Height - 2, col - (Board.Width / 2 - 1));
+            board.Place();
+        }
+        Assert.Equal(2, board.ClearRows());
     }
 }
